240c1b05ab83dddd94650888bafc8dd843893d7e
acheng <you@example.com>
2026-07-25 16:38:20 +0800

    Update legal company location

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,5 +1,5 @@
 export const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-export const companyName = "Hong Kong Leaxion Peptide Industry Co., Ltd";
+export const companyName = "Guangzhou Leaxion Peptide Industry Co., Ltd";
 
 export function assetPath(path: string) {
   return `${basePath}${path}`;
